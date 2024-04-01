54e051fc4fc48716ff69b29b5cd83b6e12eeb5d3
pedrohenmalaquias <pedromalaquias2223@gmail.com>
2024-04-01 14:57:38 -0300

    inicio form

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,91 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, reactive, computed } from 'vue'
+
+const user = reactive({
+  nome: '',
+  email: '',
+  senha: '',
+  confirmarSenha: '',
+  dataNascimento: '',
+  endereco: '',
+  cidade: '',
+  estado: [
+    'AC',
+    'AL',
+    'AP',
+    'AM',
+    'BA',
+    'CE',
+    'DF',
+    'ES',
+    'GO',
+    'MA',
+    'MT',
+    'MS',
+    'MG',
+    'PA',
+    'PB',
+    'PR',
+    'PE',
+    'PI',
+    'RJ',
+    'RN',
+    'RS',
+    'RO',
+    'RR',
+    'SC',
+    'SP',
+    'SE',
+    'TO'
+  ]
+})
+let envia = ref(false)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Insira seus dados de usuário</h1>
+    <form action="" @submit.prevent>
+      <div class="itens">
+        <input type="text" name="" id="" placeholder="Nome" v-model="user.nome" />
+      </div>
+      <div class="itens">
+        <input type="email" name="" id="" placeholder="Email" v-model="user.email" />
+      </div>
+      <div class="itens">
+        <input type="password" name="" id="" placeholder="Senha" v-model="user.senha" />
+      </div>
+      <div class="itens">
+        <input type="password" name="" id="" placeholder="Confirmar Senha" v-model="user.confirmarSenha" />
+      </div>
+      <div class="itens">
+        <input type="date" name="" id="" placeholder="Data de Nascimento" v-model="user.dataNascimento" />
+      </div>
+      <div class="itens">
+        <input type="text" name="" id="" placeholder="Endereço" v-model="user.endereco" />
+      </div>
+      <div class="itens">
+        <input type="text" name="" id="" placeholder="Cidade" v-model="user.cidade" />
+      </div>
+      <div class="itens">
+        <select name="" id="" v-model="user.estado">
+          <option v-for="estado in user.estado" :key="estado" :value="estado">{{ estado }}</option>
+        </select>
+      </div>
+      <div class="itens"><button @click="envia = true">Enviar</button></div>
+    </form>
+  </div>
+  <div v-if="envia == true" class="container corDados">
+    <div class="cabecalhoDados"><h1>Seus dados</h1> <button @click="envia = false">X</button></div>
+    <p>Nome: {{ user.nome }}</p>
+    <p>Email: {{ user.email }}</p>
+    <p>Senha: {{ user.senha }}</p>
+    <p>Confirmar Senha: {{ user.confirmarSenha }}</p>
+    <p>Data de Nascimento: {{ user.dataNascimento }}</p>
+    <p>Endereço: {{ user.endereco }}</p>
+    <p>Cidade: {{ user.cidade }}</p>
+    <p>Estado: {{ user.estado }}</p>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
